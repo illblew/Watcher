af5b3e43b8edbd7681d74e29d4ffa7ef7af4eec4
root <root@d1stkfactory>
2014-03-22 17:23:58 +0000

    First

---
+++ watcher.py
@@ -0,0 +1,21 @@
+#Timestamp it
+timestamp() {
+  echo date +"%T"
+}
+#Infinite loop
+while :
+do
+        if [ -z "$(pgrep PROCNAMEHERE)" ]
+        then
+                echo "The PROCNAMEHERE is down as of " $(date +%m%d%Y%T)
+                echo "PROCNAMEHERE went down!" $(date +%m%d%Y%T) > log$(date +%m%d%Y%T).log
+                exec /execute/thing/start.sh
+                sleep 120
+        else
+                sleep 30
+                clear
+                echo "The PROCNAMEHERE is up as of " $(date +%m%d%Y%T)
+        fi
+done
+
+#Dirty BASH by illblew
